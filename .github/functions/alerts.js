001fd24478b728553f723b955340418110f763a7
Abdulkarim <kar123580@gmail.com>
2025-12-02 04:01:04 +0400

    fixing background notification2

--- .github/functions/alerts.js
+++ .github/functions/alerts.js
@@ -78,7 +78,7 @@ async function runAlertCheck() {
               title: 'New Scholarship Alert',
               body: `Scholarship: ${scholarship["title"]} at ${scholarship["university"]} in ${scholarship["country"]} is closing in ${Math.ceil((scholarship["deadline"].toDate() - today) / (1000 * 60 * 60 * 24))} days. Apply now!`,
               priority: 'high',
-              "vibrate": [300, 500, 300]  
+              vibrate: [300, 500, 300]  
 
             }
           })
